8e37b6f57238572b88942efeb99f037a32946153
ern <siyuananior@icloud.com>
2025-06-30 11:48:42 +0800

    新增moblie条款页面

--- src/app/mobile/terms/page.tsx
+++ src/app/mobile/terms/page.tsx
@@ -1,51 +1,85 @@
 'use client'
 
-import Navbar from "@/app/pc/components/navbar";
-import Footer from "@/app/pc/components/footer";
+import Navbar from "@/app/mobile/components/navbar";
+import Footer from "@/app/mobile/components/footer";
 import { useLanguage } from "@/context/languageContext";
 
 const Terms = () => {
   const { t } = useLanguage();
 
   return (
-    <div className="bg-white min-w-7xl">
+    <div className="bg-white">
       <Navbar />
-      <div className="flex flex-col items-center text-stone-500 p-30">
+      <div className="flex flex-col items-center text-stone-500 p-5">
         <div className="leading-10">
-          <h3 className="mb-10 text-4xl font-bold text-black text-center">{t("SERVICE")}</h3>
-          <div >We are committed to ensuring the privacy and security of your personal information. This Privacy Policy outlines how we collect, use and protect your data. By using our Services, you agree to be bound by the terms and practices described here</div>
-          <div>1. Information we collect</div>
-          <div>We may collect the following types of information:</div>
-          <div>Personal information: name, email address, contact information.</div>
-          <div>Design Preferences: Information you provide about your design needs and preferences.</div>
-          <div>Usage Data: Interaction data, such as your interactions with our websites and design tools.</div>
-          <div>We collect this information to provide you with personalized and efficient graphic design solutions.</div>
-          <div>2. How we use your information</div>
-          <div>We use your information to:</div>
-          <div>Create and deliver graphic designs based on your preferences.</div>
-          <div>Communicate with you about your design projects and updates.</div>
-          <div>Usage Data: Interaction data, such as your interactions with our websites and design tools.</div>
-          <div>3. Data security</div>
-          <div>We take the security of your data seriously. We take technical and organizational measures to protect your information from unauthorized access, loss, misuse or alteration. However, please note that the transmission of data over the Internet is No method is completely safe.</div>
-          <div>4. Information Sharing</div>
-          <div>We do not share your personal information with third parties for marketing purposes. However, we may share your information with:</div>
-          <div>Service providers who assist us in providing design services.</div>
-          <div>Legal and regulatory authorities as required by law.</div>
-          <div>5. Cookies and Tracking Technologies</div>
-          <div>Our websites and services may use cookies and similar tracking technologies to enhance your browsing experience and collect usage information. You can adjust your browser settings to manage your cookie preferences.</div>
-          <div>6. Your Choices</div>
-          <div>You have the following rights:</div>
-          <div>Access, correct, or delete your personal information.</div>
-          <div>Opt out of receiving marketing communications.</div>
-          <div>Raise an objection or restrict data processing.</div>
-          <div>To exercise these rights, please contact us at enquiry@kainoai.com.</div>
-          <div>7. Children's Privacy</div>
-          <div>Our Services are not directed to individuals under [12] years of age. We do not knowingly collect personal information from children.</div>
-          <div>8. Changes to Privacy Policy</div>
-          <div>We may periodically update this Privacy Policy to reflect changes to our practices and services. Revisions will be posted on our website along with the effective date.</div>
-          <div>For any inquiries about our privacy policy or data practices, please contact us at enquiry@kainoai.com. Your privacy is important to us and we are committed to addressing any concerns you may have.</div>
-          <div>KainoAI Operation Team</div>
-          <div>Contact email:enquiry@kainoai.com</div>
+          <h3 className="text-lg font-bold text-black text-center">{t("TERMS_TITLE")}</h3>
+          <div className="mb-10 text-center text-xs">{t('TERMS_LAST_UPDATED')}</div>
+          <div className="text-sm">{t('TERMS_INTRO')}</div>
+
+          <div className="text-sm text-black mt-10" >{t('TERMS_SECTION1_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION1_BODY')}</div>
+
+          <div className="text-sm text-black mt-10" >{t('TERMS_SECTION2_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION2_BODY')}</div>
+          <ul>
+            <li className="text-sm">{t('TERMS_SECTION2_RULE1')}</li>
+            <li className="text-sm">{t('TERMS_SECTION2_RULE2')}</li>
+            <li className="text-sm">{t('TERMS_SECTION2_RULE3')}</li>
+            <li className="text-sm">{t('TERMS_SECTION2_RULE4')}</li>
+          </ul>
+
+          <div className="text-sm text-black mt-10" >{t('TERMS_SECTION3_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION3_BODY')}</div>
+
+          <div className="text-sm text-black mt-10" >{t('TERMS_SECTION4_TITLE')}</div>
+
+          <div className="text-sm text-black mt-4" >{t('TERMS_SECTION4_1_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_1_BODY')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_1_RULE_1')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_1_RULE_2')}</div>
+
+          <div className="text-sm text-black mt-4" >{t('TERMS_SECTION4_2_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_2_BODY')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_2_RULE_1')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_2_RULE_2')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_2_RULE_3')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_2_RULE_4')}</div>
+
+          <div className="text-sm text-black mt-4" >{t('TERMS_SECTION4_3_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_3_BODY')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_3_RULE_1')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_3_RULE_2')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_3_RULE_3')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_3_RULE_4')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_3_RULE_5')}</div>
+
+          <div className="text-sm text-black mt-4" >{t('TERMS_SECTION4_4_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_4_BODY')}</div>
+
+
+          <div className="text-sm text-black mt-4" >{t('TERMS_SECTION4_5_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_5_BODY')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_5_RULE_1')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_5_RULE_2')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_5_RULE_3')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_5_RULE_4')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_5_RULE_5')}</div>
+
+          <div className="text-sm text-black mt-4" >{t('TERMS_SECTION4_6_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION4_6_BODY')}</div>
+
+          <div className="text-sm text-black mt-10" >{t('TERMS_SECTION5_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION5_BODY')}</div>
+
+          <div className="text-sm text-black mt-10" >{t('TERMS_SECTION6_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION6_BODY')}</div>
+          <div className="text-sm">{t('TERMS_SECTION6_RULE_1')}</div>
+          <div className="text-sm">{t('TERMS_SECTION6_RULE_2')}</div>
+          <div className="text-sm">{t('TERMS_SECTION6_RULE_3')}</div>
+
+          <div className="text-sm text-black mt-10" >{t('TERMS_SECTION7_TITLE')}</div>
+          <div className="text-sm">{t('TERMS_SECTION7_BODY')}</div>
+
         </div>
         
       </div>
--- src/app/pc/terms/page.tsx
+++ src/app/pc/terms/page.tsx
@@ -14,71 +14,71 @@ const Terms = () => {
         <div className="leading-10">
           <h3 className="text-4xl font-bold text-black text-center">{t("TERMS_TITLE")}</h3>
           <div className="mb-10 text-center">{t('TERMS_LAST_UPDATED')}</div>
-<div>{t('TERMS_INTRO')}</div>
+        <div>{t('TERMS_INTRO')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION1_TITLE')}</div>
-<div>{t('TERMS_SECTION1_BODY')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION1_TITLE')}</div>
+        <div>{t('TERMS_SECTION1_BODY')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION2_TITLE')}</div>
-<div>{t('TERMS_SECTION2_BODY')}</div>
-<ul>
-  <li>{t('TERMS_SECTION2_RULE1')}</li>
-  <li>{t('TERMS_SECTION2_RULE2')}</li>
-  <li>{t('TERMS_SECTION2_RULE3')}</li>
-  <li>{t('TERMS_SECTION2_RULE4')}</li>
-</ul>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION2_TITLE')}</div>
+        <div>{t('TERMS_SECTION2_BODY')}</div>
+        <ul>
+          <li>{t('TERMS_SECTION2_RULE1')}</li>
+          <li>{t('TERMS_SECTION2_RULE2')}</li>
+          <li>{t('TERMS_SECTION2_RULE3')}</li>
+          <li>{t('TERMS_SECTION2_RULE4')}</li>
+        </ul>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION3_TITLE')}</div>
-<div>{t('TERMS_SECTION3_BODY')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION3_TITLE')}</div>
+        <div>{t('TERMS_SECTION3_BODY')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_TITLE')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_TITLE')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_1_TITLE')}</div>
-<div>{t('TERMS_SECTION4_1_BODY')}</div>
-<div>{t('TERMS_SECTION4_1_RULE_1')}</div>
-<div>{t('TERMS_SECTION4_1_RULE_2')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_1_TITLE')}</div>
+        <div>{t('TERMS_SECTION4_1_BODY')}</div>
+        <div>{t('TERMS_SECTION4_1_RULE_1')}</div>
+        <div>{t('TERMS_SECTION4_1_RULE_2')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_2_TITLE')}</div>
-<div>{t('TERMS_SECTION4_2_BODY')}</div>
-<div>{t('TERMS_SECTION4_2_RULE_1')}</div>
-<div>{t('TERMS_SECTION4_2_RULE_2')}</div>
-<div>{t('TERMS_SECTION4_2_RULE_3')}</div>
-<div>{t('TERMS_SECTION4_2_RULE_4')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_2_TITLE')}</div>
+        <div>{t('TERMS_SECTION4_2_BODY')}</div>
+        <div>{t('TERMS_SECTION4_2_RULE_1')}</div>
+        <div>{t('TERMS_SECTION4_2_RULE_2')}</div>
+        <div>{t('TERMS_SECTION4_2_RULE_3')}</div>
+        <div>{t('TERMS_SECTION4_2_RULE_4')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_3_TITLE')}</div>
-<div>{t('TERMS_SECTION4_3_BODY')}</div>
-<div>{t('TERMS_SECTION4_3_RULE_1')}</div>
-<div>{t('TERMS_SECTION4_3_RULE_2')}</div>
-<div>{t('TERMS_SECTION4_3_RULE_3')}</div>
-<div>{t('TERMS_SECTION4_3_RULE_4')}</div>
-<div>{t('TERMS_SECTION4_3_RULE_5')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_3_TITLE')}</div>
+        <div>{t('TERMS_SECTION4_3_BODY')}</div>
+        <div>{t('TERMS_SECTION4_3_RULE_1')}</div>
+        <div>{t('TERMS_SECTION4_3_RULE_2')}</div>
+        <div>{t('TERMS_SECTION4_3_RULE_3')}</div>
+        <div>{t('TERMS_SECTION4_3_RULE_4')}</div>
+        <div>{t('TERMS_SECTION4_3_RULE_5')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_4_TITLE')}</div>
-<div>{t('TERMS_SECTION4_4_BODY')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_4_TITLE')}</div>
+        <div>{t('TERMS_SECTION4_4_BODY')}</div>
 
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_5_TITLE')}</div>
-<div>{t('TERMS_SECTION4_5_BODY')}</div>
-<div>{t('TERMS_SECTION4_5_RULE_1')}</div>
-<div>{t('TERMS_SECTION4_5_RULE_2')}</div>
-<div>{t('TERMS_SECTION4_5_RULE_3')}</div>
-<div>{t('TERMS_SECTION4_5_RULE_4')}</div>
-<div>{t('TERMS_SECTION4_5_RULE_5')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_5_TITLE')}</div>
+        <div>{t('TERMS_SECTION4_5_BODY')}</div>
+        <div>{t('TERMS_SECTION4_5_RULE_1')}</div>
+        <div>{t('TERMS_SECTION4_5_RULE_2')}</div>
+        <div>{t('TERMS_SECTION4_5_RULE_3')}</div>
+        <div>{t('TERMS_SECTION4_5_RULE_4')}</div>
+        <div>{t('TERMS_SECTION4_5_RULE_5')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_6_TITLE')}</div>
-<div>{t('TERMS_SECTION4_6_BODY')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION4_6_TITLE')}</div>
+        <div>{t('TERMS_SECTION4_6_BODY')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION5_TITLE')}</div>
-<div>{t('TERMS_SECTION5_BODY')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION5_TITLE')}</div>
+        <div>{t('TERMS_SECTION5_BODY')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION6_TITLE')}</div>
-<div>{t('TERMS_SECTION6_BODY')}</div>
-<div>{t('TERMS_SECTION6_RULE_1')}</div>
-<div>{t('TERMS_SECTION6_RULE_2')}</div>
-<div>{t('TERMS_SECTION6_RULE_3')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION6_TITLE')}</div>
+        <div>{t('TERMS_SECTION6_BODY')}</div>
+        <div>{t('TERMS_SECTION6_RULE_1')}</div>
+        <div>{t('TERMS_SECTION6_RULE_2')}</div>
+        <div>{t('TERMS_SECTION6_RULE_3')}</div>
 
-<div className="text-lg text-black mt-10" >{t('TERMS_SECTION7_TITLE')}</div>
-<div>{t('TERMS_SECTION7_BODY')}</div>
+        <div className="text-lg text-black mt-10" >{t('TERMS_SECTION7_TITLE')}</div>
+        <div>{t('TERMS_SECTION7_BODY')}</div>
 
         </div>
         
